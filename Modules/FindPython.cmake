d6560768d5cdaa7935b5a87490086ec6cabd8330
Branan Purvine-Riley <branan@gmail.com>
2010-07-22 22:30:14 -0700

    Add library and include dir to FindPython

--- Modules/FindPython.cmake
+++ Modules/FindPython.cmake
@@ -3,8 +3,10 @@
 # and determines where they are located
 #
 #
-#  Python_FOUND - The requested Python components were found
-#  Python_EXECUTABLE  - path to the Python interpreter
+#  PYTHON_FOUND - The requested Python components were found
+#  PYTHON_EXECUTABLE  - path to the Python interpreter
+#  PYTHON_INCLUDE_DIRS - path to the Python header files
+#  PYTHON_LIBRARIES - the libraries to link against for python
 #
 
 #=============================================================================
@@ -21,82 +23,97 @@
 #  License text for the above reference.)
 
 IF("3" STREQUAL "${Python_FIND_VERSION_MAJOR}")
-  SET(Python_3_OK "TRUE")
-  SET(Python_2_OK "FALSE") # redundant in version selection code, but skips a FOREACH
+  SET(PYTHON_3_OK "TRUE")
+  SET(PYTHON_2_OK "FALSE") # redundant in version selection code, but skips a FOREACH
 ELSE("3" STREQUAL "${Python_FIND_VERSION_MAJOR}")
-  SET(Python_2_OK "TRUE")
-  # don't set Python_3_OK to false here - if the user specified it we want to search for Python 2 & 3
+  SET(PYTHON_2_OK "TRUE")
+  # don't set PYTHON_3_OK to false here - if the user specified it we want to search for Python 2 & 3
 ENDIF("3" STREQUAL "${Python_FIND_VERSION_MAJOR}")
 
 # This is  heavily inspired by FindBoost.cmake, with the addition of a second version list to keep
 # python 2 and python 3 separate
 IF(Python_FIND_VERSION_EXACT)
-  SET(_Python_TEST_VERSIONS "${Python_FIND_VERSION_MAJOR}.${Python_FIND_VERSION_MINOR}")
+  SET(_PYTHON_TEST_VERSIONS "${Python_FIND_VERSION_MAJOR}.${Python_FIND_VERSION_MINOR}")
 ELSE(Python_FIND_VERSION_EXACT)
-  SET(_Python_3_KNOWN_VERSIONS ${Python_3_ADDITIONAL_VERSIONS}
+  SET(_PYTHON_3_KNOWN_VERSIONS ${PYTHON_3_ADDITIONAL_VERSIONS}
     "3.1" "3.0")
-  SET(_Python_2_KNOWN_VERSIONS ${Python_2_ADDITIONAL_VERSIONS}
+  SET(_PYTHON_2_KNOWN_VERSIONS ${PYTHON_2_ADDITIONAL_VERSIONS}
     "2.7" "2.6" "2.5" "2.4" "2.3" "2.2" "2.1" "2.0" "1.6" "1.5")
-  SET(_Python_TEST_VERSIONS)
+  SET(_PYTHON_TEST_VERSIONS)
   IF(Python_FIND_VERSION)
-    IF(Python_3_OK)
-      FOREACH(version ${_Python_3_KNOWN_VERSIONS})
+    IF(PYTHON_3_OK)
+      FOREACH(version ${_PYTHON_3_KNOWN_VERSIONS})
         IF(NOT ${version} VERSION_LESS ${Python_FIND_VERSION})
-          LIST(APPEND _Python_TEST_VERSIONS ${version})
+          LIST(APPEND _PYTHON_TEST_VERSIONS ${version})
         ENDIF(NOT ${version} VERSION_LESS ${Python_FIND_VERSION})
       ENDFOREACH(version)
-    ENDIF(Python_3_OK)
-    IF(Python_2_OK)
-      FOREACH(version ${_Python_2_KNOWN_VERSIONS})
+    ENDIF(PYTHON_3_OK)
+    IF(PYTHON_2_OK)
+      FOREACH(version ${_PYTHON_2_KNOWN_VERSIONS})
         IF(NOT ${version} VERSION_LESS ${Python_FIND_VERSION})
-          LIST(APPEND _Python_TEST_VERSIONS ${version})
+          LIST(APPEND _PYTHON_TEST_VERSIONS ${version})
         ENDIF(NOT ${version} VERSION_LESS ${Python_FIND_VERSION})
       ENDFOREACH(version)
-    ENDIF(Python_2_OK)
+    ENDIF(PYTHON_2_OK)
   ELSE(Python_FIND_VERSION)
-    IF(Python_3_OK)
-      LIST(APPEND _Python_TEST_VERSIONS ${_Python_3_KNOWN_VERSIONS})
-    ENDIF(Python_3_OK)
-    IF(Python_2_OK)
-      LIST(APPEND _Python_TEST_VERSIONS ${_Python_2_KNOWN_VERSIONS})
-    ENDIF(Python_2_OK)
+    IF(PYTHON_3_OK)
+      LIST(APPEND _PYTHON_TEST_VERSIONS ${_PYTHON_3_KNOWN_VERSIONS})
+    ENDIF(PYTHON_3_OK)
+    IF(PYTHON_2_OK)
+      LIST(APPEND _PYTHON_TEST_VERSIONS ${_PYTHON_2_KNOWN_VERSIONS})
+    ENDIF(PYTHON_2_OK)
   ENDIF(Python_FIND_VERSION)
 ENDIF(Python_FIND_VERSION_EXACT)
 
-SET(_Python_EXE_VERSIONS)
-FOREACH(version ${_Python_TEST_VERSIONS})
-  LIST(APPEND _Python_EXE_VERSIONS python${version})
-ENDFOREACH(version ${_Python_TEST_VERSIONS})
+SET(_PYTHON_EXE_VERSIONS)
+FOREACH(version ${_PYTHON_TEST_VERSIONS})
+  LIST(APPEND _PYTHON_EXE_VERSIONS python${version})
+ENDFOREACH(version ${_PYTHON_TEST_VERSIONS})
 
 IF(WIN32)
-  SET(_Python_REGISTRY_KEYS)
-  FOREACH(version ${_Python_TEST_VERSIONS})
-    LIST(APPEND _Python_REGISTRY_KEYS [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\${version}\\InstallPath])
-  ENDFOREACH(version ${_Python_TEST_VERSIONS})
+  SET(_PYTHON_REGISTRY_KEYS)
+  FOREACH(version ${_PYTHON_TEST_VERSIONS})
+    LIST(APPEND _PYTHON_REGISTRY_KEYS [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\${version}\\InstallPath])
+  ENDFOREACH(version ${_PYTHON_TEST_VERSIONS})
   # this will find any standard windows Python install before it finds anything from Cygwin
-  FIND_PROGRAM(Python_EXECUTABLE NAMES python ${_Python_EXE_VERSIONS} PATHS ${_Python_REGISTRY_KEYS})
+  FIND_PROGRAM(PYTHON_EXECUTABLE NAMES python ${_PYTHON_EXE_VERSIONS} PATHS ${_PYTHON_REGISTRY_KEYS})
 ELSE(WIN32)
-  FIND_PROGRAM(Python_EXECUTABLE NAMES ${_Python_EXE_VERSIONS} python)
+  FIND_PROGRAM(PYTHON_EXECUTABLE NAMES ${_PYTHON_EXE_VERSIONS} python PATHS)
 ENDIF(WIN32)
+
+EXECUTE_PROCESS(COMMAND "${PYTHON_EXECUTABLE}" "-c" "from sys import *; stdout.write(str(version_info[0])+'.'+str(version_info[1]))" OUTPUT_VARIABLE _PYTHON_VERSION)
 
 
 # Make sure our python version matches the requested version when we get a non-versioned executable name
-GET_FILENAME_COMPONENT(_Python_EXENAME ${Python_EXECUTABLE} NAME_WE)
-IF(Python_FIND_VERSION AND Python_EXECUTABLE AND ${_Python_EXENAME} STREQUAL "python")
-  EXEC_PROGRAM("${Python_EXECUTABLE}" ARGS "--version" OUTPUT_VARIABLE _Python_VERSION)
-  STRING(SUBSTRING "${_Python_VERSION}" 7 3 _Python_VERSION)
+GET_FILENAME_COMPONENT(_PYTHON_EXENAME ${PYTHON_EXECUTABLE} NAME_WE)
+IF(Python_FIND_VERSION AND PYTHON_EXECUTABLE AND ${_PYTHON_EXENAME} STREQUAL "python")
+  STRING(SUBSTRING "${_PYTHON_VERSION}" 7 3 _PYTHON_VERSION)
   IF(Python_FIND_VERSION_EXACT)
-    IF(NOT ${_Python_VERSION} VERSION_EQUAL "${Python_FIND_VERSION_MAJOR}.${Python_FIND_VERSION_MINOR}")
-      SET(Python_EXECUTABLE Python_EXECUTABLE-NOTFOUND)
-    ENDIF(NOT ${_Python_VERSION} VERSION_EQUAL "${Python_FIND_VERSION_MAJOR}.${Python_FIND_VERSION_MINOR}")
+    IF(NOT ${_PYTHON_VERSION} VERSION_EQUAL "${Python_FIND_VERSION_MAJOR}.${Python_FIND_VERSION_MINOR}")
+      SET(PYTHON_EXECUTABLE PYTHON_EXECUTABLE-NOTFOUND)
+    ENDIF(NOT ${_PYTHON_VERSION} VERSION_EQUAL "${Python_FIND_VERSION_MAJOR}.${Python_FIND_VERSION_MINOR}")
   ELSE(Python_FIND_VERSION_EXACT)
-    IF(NOT ${_Python_VERSION} VERSION_LESS ${Python_FIND_VERSION})
-      SET(Python_EXECUTABLE Python_EXECUTABLE-NOTFOUND)
-    ENDIF(NOT ${_Python_VERSION} VERSION_LESS ${Python_FIND_VERSION})
+    IF(NOT ${_PYTHON_VERSION} VERSION_LESS ${Python_FIND_VERSION})
+      SET(PYTHON_EXECUTABLE PYTHON_EXECUTABLE-NOTFOUND)
+    ENDIF(NOT ${_PYTHON_VERSION} VERSION_LESS ${Python_FIND_VERSION})
   ENDIF(Python_FIND_VERSION_EXACT)
-ENDIF(Python_FIND_VERSION AND Python_EXECUTABLE AND ${_Python_EXENAME} STREQUAL "python")
+ENDIF(Python_FIND_VERSION AND PYTHON_EXECUTABLE AND ${_PYTHON_EXENAME} STREQUAL "python")
+
+# Get our library path and include directory from python
+EXECUTE_PROCESS(COMMAND "${PYTHON_EXECUTABLE}" "-c" "from sys import *; stdout.write(prefix)" OUTPUT_VARIABLE _PYTHON_PREFIX)
+EXECUTE_PROCESS(COMMAND "${PYTHON_EXECUTABLE}" "-c" "from sys import *; stdout.write(exec_prefix)" OUTPUT_VARIABLE _PYTHON_EPREFIX)
+
+
+set(PYTHON_INCLUDE_DIRS "${_PYTHON_PREFIX}/include/python${_PYTHON_VERSION}")
+set(PYTHON_LIBRARIES "${_PYTHON_EPREFIX}/lib/${CMAKE_SHARED_MODULE_PREFIX}python${_PYTHON_VERSION}${CMAKE_SHARED_MODULE_SUFFIX}")
 
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Python DEFAULT_MSG Python_EXECUTABLE)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Python DEFAULT_MSG PYTHON_EXECUTABLE PYTHON_INCLUDE_DIRS PYTHON_LIBRARIES)
 
-MARK_AS_ADVANCED(Python_EXECUTABLE)
+MARK_AS_ADVANCED(PYTHON_EXECUTABLE)
+MARK_AS_ADVANCED(PYTHON_INCLUDE_DIRS)
+MARK_AS_ADVANCED(PYTHON_LIBRARIES)
+
+MESSAGE(${PYTHON_INCLUDE_DIRS})
+MESSAGE(${PYTHON_LIBRARIES})
+MESSAGE(${PYTHON_EXECUTABLE})
